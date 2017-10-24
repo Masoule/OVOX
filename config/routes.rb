357a6f831c61418d5748deea06c972051cf0468c
salome siavoshi <salomei@gmail.com>
2017-10-24 12:34:49 -0400

    added user model, contorllers, jbuilder views, apiUtil

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'sessions/new'
+  end
+
+  namespace :api do
+    get 'sessions/create'
+  end
+
+  get 'users/new'
+
+  get 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
