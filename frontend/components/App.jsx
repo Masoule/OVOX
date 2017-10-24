8e92bd1a22af1d86d3e61f944a91e478a8000027
salome siavoshi <salomei@gmail.com>
2017-10-24 17:33:41 -0400

    fixes bugs

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { AuthRoute } from '../util/route_util';
-import { Form , Buttons} from './session/session_form_container';
+import SessionFormContainer from './session/session_form_container';
 
 const App = () => (
   <div>
 
     <header>
       <h1>OVOX</h1>
-      <Buttons />
     </header>
 
-
-    <AuthRoute path="/login" component={Form} />
-    <AuthRoute path="/signup" component={Form} />
+    <AuthRoute path="/login" component={SessionFormContainer} />
+    <AuthRoute path="/signup" component={SessionFormContainer} />
 
   </div>
 );
--- frontend/components/session/session_form_container.js
+++ frontend/components/session/session_form_container.js
@@ -1,34 +1,33 @@
 import SessionForm from './session_form';
-import SessionButtons from './session_buttons';
 import { connect } from 'react-redux';
-import { login , logout, signUp } from '../../actions/session_actions';
+import {login , signUp} from '../../actions/session_actions';
 import { withRouter } from 'react-router-dom';
+
 
 const mapStateToProps = (state, ownProps) => {
   const formType = ownProps.match.path === "/login" ? "Login" : "Sign Up";
   const loggedIn = state.session.currentUser ? true : false;
   return {
     errors: state.errors.session,
-    currentUser: state.session.currentUser,
     formType,
-    loggedIn,
+    loggedIn
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   const action = ownProps.match.path === "/login" ? login : signUp;
   return {
-    processForm: (user) => dispatch(action(user)),
-    logout: () => dispatch(logout()),
+    processForm: (user) => dispatch(action(user))
   };
 };
 
-export const Form =  withRouter(connect(
+
+export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
 )(SessionForm));
 
-export const Buttons =  connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SessionButtons);
+// export const Buttons =  connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(SessionButtons);
